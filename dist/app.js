020d12639ca5cba93efed1c9e2cd0fc98d67520f
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2023-09-15 09:41:45 +0600

    vercel update

--- dist/app.js
+++ dist/app.js
@@ -18,4 +18,7 @@ app.use("/api/v1", user_route_1.UserRoute);
 app.use("/api/v1/categories", category_route_1.CategoryRoute);
 app.use("/api/v1/books", book_route_1.BookRoute);
 app.use("/api/v1/orders", order_route_1.OrderRoute);
+app.get("/", (req, res) => {
+    res.send("Welcome to the Bookstore API");
+});
 exports.default = app;
