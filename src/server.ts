b1e0ba9fc1c459dd0cba2a92426cb293020776ab
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2023-09-15 09:47:45 +0600

    main fn added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import app from "./app";
 const port = Number(process.env.PORT) | 5000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+const main = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+main();
